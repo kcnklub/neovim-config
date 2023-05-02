6ccf7e3ddc99c15fd5d6023f354e9fcbc1bbd452
kymiller <kylemiller457@gmail.com>
2023-05-01 21:54:19 -0500

    Changing color scheme again

--- lua/plugins/colorscheme/init.lua
+++ lua/plugins/colorscheme/init.lua
@@ -1,44 +1,14 @@
 return {
-    {
-        "folke/styler.nvim",
-        event = "VeryLazy",
-        config = function()
-            require("styler").setup({
-                themes = {
-                    markdown = { colorscheme = "gruvbox" },
-                    help = { colorscheme = "gruvbox" },
-                },
-            })
-        end,
-    },
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {
-            style = "storm",
-            transparent = true,
-            styles = {
-                sidebars = "transparent",
-                floats = "transparent",
-            },
-        },
-        config = function(_, opts)
-            local tokyonight = require("tokyonight")
-            tokyonight.setup(opts)
-            tokyonight.load()
-        end,
-    },
-    {
-        "catppuccin/nvim",
-        lazy = false,
-        name = "catppuccin"
-    },
-    {
-        "ellisonleao/gruvbox.nvim",
-        lazy = false,
-        config = function()
-            require("gruvbox").setup()
-        end
-    }
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		config = function()
+			require("catppuccin").setup({
+				transparent_background = true,
+				flavour = "mocha",
+			})
+			vim.cmd([[colorscheme catppuccin]])
+		end,
+	},
 }
