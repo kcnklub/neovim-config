4cc35c9117846296a24e6d2f797e6f6c3fe8ea05
kymiller <kylemiller457@gmail.com>
2024-06-03 13:23:25 -0500

    updating config

--- lua/plugins/colorscheme/init.lua
+++ lua/plugins/colorscheme/init.lua
@@ -1,9 +1,28 @@
 return {
-	{
-		"ellisonleao/gruvbox.nvim",
-		lazy = false,
-		config = function()
-			vim.cmd([[colorscheme gruvbox]])
-		end,
-	},
+    {
+        "ellisonleao/gruvbox.nvim",
+        lazy = false,
+        config = function()
+            ---vim.cmd([[colorscheme gruvbox]])
+        end,
+    },
+    {
+        "rose-pine/neovim",
+        lazy = false,
+        config = function()
+            require("rose-pine").setup({
+                variant = "moon",
+                dark_variant = "moon",
+                dim_inactitve_window = true,
+            })
+            ---vim.cmd([[colorscheme rose-pine]])
+        end,
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        config = function()
+            vim.cmd([[colorscheme tokyonight-storm]])
+        end,
+    },
 }
