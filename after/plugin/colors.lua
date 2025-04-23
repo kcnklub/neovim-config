de217d43ad78c6cc4d579aefee593b6028c74f0d
kymiller <kylemiller457@gmail.com>
2025-04-23 17:28:57 -0500

    fixing avante

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,4 +1,5 @@
 require("gruvbox").setup({
     transparent_mode = true,
 })
+vim.o.background = "dark"
 vim.cmd("colorscheme gruvbox")
--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -5,14 +5,9 @@ return {
     opts = {
         -- add any opts here
         -- for example
-        provider = "gemini",
-        gemini = {
-            endpoint = "https://generativelanguage.googleapis.com",
-            model = "gemini-2.5-pro-exp-03-25",
-            timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-            temperature = 0,
-            max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-            --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+        provider = "vertex",
+        vertex = {
+            model = "gemini-2.5-flash-preview-04-17",
         },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
