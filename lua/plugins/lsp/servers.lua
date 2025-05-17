caa90682da1bdf142a95d3815f8697faf3fe46a5
kyle <kyle@pop-os.attlocal.net>
2025-05-17 07:48:57 -0500

    Removing deprecated function call from mason-lspconfig

--- lua/plugins/lsp/servers.lua
+++ lua/plugins/lsp/servers.lua
@@ -6,9 +6,9 @@ local icons = require("config.icons")
 local function lsp_init()
     local signs = {
         { name = "DiagnosticSignError", text = icons.diagnostics.Error },
-        { name = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
-        { name = "DiagnosticSignHint", text = icons.diagnostics.Hint },
-        { name = "DiagnosticSignInfo", text = icons.diagnostics.Info },
+        { name = "DiagnosticSignWarn",  text = icons.diagnostics.Warning },
+        { name = "DiagnosticSignHint",  text = icons.diagnostics.Hint },
+        { name = "DiagnosticSignInfo",  text = icons.diagnostics.Info },
     }
     for _, sign in ipairs(signs) do
         vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
@@ -60,22 +60,6 @@ function M.setup(_, opts)
 
     local servers = opts.servers
     require("mason-lspconfig").setup({ ensure_installed = vim.tbl_keys(servers) })
-    require("mason-lspconfig").setup_handlers({
-        function(server)
-            local server_opts = servers[server] or {}
-            server_opts.capabilities = lsp_utils.capabilities()
-            if opts.setup[server] then
-                if opts.setup[server](server, server_opts) then
-                    return
-                end
-            elseif opts.setup["*"] then
-                if opts.setup["*"](server, server_opts) then
-                    return
-                end
-            end
-            require("lspconfig")[server].setup(server_opts)
-        end,
-    })
 end
 
 return M
